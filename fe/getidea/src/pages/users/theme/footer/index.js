11039bb337d7b32619778ffbdbf5846bc3298dc5
vktmk123 <patheonmk@gmail.com>
2024-03-18 23:05:33 +0700

    Revert "header"
    This reverts commit c24754434fcdebc71528f238e75ba5905604bd18.

--- fe/getidea/src/pages/users/theme/footer/index.js
+++ fe/getidea/src/pages/users/theme/footer/index.js
@@ -1,10 +1,11 @@
 import {memo} from 'react';
-import './style.scss';
 
 const Footer = () => {
-    return <Footer>
-        
-    </Footer>;
+    return (
+    <>
+        <h1>Footer</h1>
+    </> 
+    );
 };
 
 export default memo(Footer);
