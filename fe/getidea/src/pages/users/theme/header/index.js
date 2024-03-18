c803ed4c42f9ea365407fb6ab907f70ce7a53e8b
vktmk123 <patheonmk@gmail.com>
2024-03-18 16:51:35 +0700

    tao hetder

--- fe/getidea/src/pages/users/theme/header/index.js
+++ fe/getidea/src/pages/users/theme/header/index.js
@@ -1,59 +1,139 @@
-import { memo } from 'react';
-import {AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineUser } from 'react-icons/ai';
+import { memo, useState } from 'react';
+import { AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import './style.scss';
 import { formatter } from 'utils/formater';
+import { ROUTERS } from 'utils/router';
+
 
 const Header = () => {
+    const [menus, setMenus] = useState([
+        {
+            name:"trang chu",
+            path:ROUTERS.USER.HOME,
+        },
+        {
+            name:"cuong hang",
+            path:ROUTERS.USER.PRODUCTS,  
+        },
+        {
+            name:"San Pham",
+            path:"", 
+            isShowSubmenu:false,
+            child:[
+                {
+                    name:"thit",
+                    path:"",
+                },
+                {
+                    name:"rau",
+                    path:"",
+                },
+                {
+                    name:"qua",
+                    path:"",
+                },
+            ]
+        },
+        {
+            name:"bai viet",
+            path:"",  
+        },
+        {
+            name:"lien he",
+            path:"",  
+        },
+    ]);
+
     return (
-        <div className='header_top'>
-            <div className="container">
-                <div className='row'>
-                    <div className='col-6 header_top_left'>
-                        <ul>
-                            <li>
-                                <AiOutlineMail/>
-                                thanh@gmail.com
+        <>
+            <div className='header_top'>
+                <div className="container">
+                    <div className='row'>
+                        <div className='col-6 header_top_left'>
+                            <ul>
+                                <li>
+                                    <AiOutlineMail />
+                                    thanh@gmail.com
                                 </li>
-                            <li>Hello i love you {formatter(3000)}</li>
-                        </ul>
-                    </div>
-                    <div className='col-6 header_top_right'>
-                        <ul>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineFacebook />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineInstagram />
-                                </Link>
+                                <li>Hello i love you {formatter(3000)}</li>
+                            </ul>
+                        </div>
+                        <div className='col-6 header_top_right'>
+                            <ul>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineFacebook />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineInstagram />
+                                    </Link>
 
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineLinkedin />
-                                </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineLinkedin />
+                                    </Link>
 
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineGlobal />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={""}>
-                                    <AiOutlineUser />
-                                </Link>
-                                <span>Đăng nhập</span>
-                            </li>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineGlobal />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineUser />
+                                    </Link>
+                                    <span>Đăng nhập</span>
+                                </li>
 
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-xl-3'>
+                        <div className='header_logo'>
+                            <h1>VT Shop</h1>
+                        </div>
+                    </div>
+                    <div className='col-xl-6'> 
+                        <nav className='header_menu'>
+                            <ul>
+                                {
+                                menus?.map((menu, menuKey) => (
+                                    <li key={menuKey} className={menuKey === 0 ? 'active' : ""}>
+                                        <Link to={menu?.path}>{menu?.name}</Link>
+                                    </li>
+                                ))} 
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className='col-xl-3'>
+                        <div className='header_cart'>
+                            <div className='header_cart_price'>
+                                <span>{formatter(1000232)}</span>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link to=""> <AiOutlineShoppingCart/> <span>10</span> </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+
     );
 };
 
